98e28915812c15bf3a7ca2ecadbd42286767d3f8
ninz <nreclarin@gmail.coml>
2023-01-09 15:34:10 +1100

    initial commit

---
+++ src/data_classes.py
@@ -0,0 +1,22 @@
+"""
+Data containers.
+"""
+from dataclasses import dataclass
+
+
+@dataclass(repr=True, eq=True)
+class Rectangle:
+    xmin: float
+    xmax: float
+    ymin: float
+    ymax: float
+
+
+@dataclass(repr=True, eq=True)
+class LabeledData:
+    img_id: str
+    img_data: np.ndarray
+    damage_tiles: list
+    sampled_non_damage_tiles: list
+    non_damage_tiles: list
+    annotations: list
