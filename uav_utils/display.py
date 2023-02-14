81ee940909d59f01f958806923064e6f5900b73c
ninz <nreclarin@gmail.com>
2023-02-14 23:29:50 +0800

    show heatmap

--- uav_utils/display.py
+++ uav_utils/display.py
@@ -54,7 +54,7 @@ def display_annotations(img: np.ndarray, boxes: list, target_label: str = "damag
 def show_result(fname: str, no_island: bool, settings: AnalysisSettings, ds: DisplaySettings):
     """
     Get result for image display.
-    
+
     Parameters
     ----------
     fname :
